c9e7c4d534cad91bc788b036e02e24836459719b
Fulzadrot <fulzafrot@mail.ru>
2020-11-30 13:54:03 +0300

    Initial commit

---
+++ src/Server/AuthService.java
@@ -0,0 +1,9 @@
+package Server;
+
+public interface AuthService {
+
+    /**
+     * @return - nickname если пользоатель есть и null если пользователя нет
+     */
+    String getNicknameByLoginAndPassword(String login, String password);
+}
